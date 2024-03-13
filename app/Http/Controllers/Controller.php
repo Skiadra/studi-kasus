8f4ead3efe889bdbdaea7aafab3e4788bd1e9fc6
Skiadra <riadywiguna5@gmail.com>
2024-03-13 21:49:10 +0700

    firstcommit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Http\Controllers\Controller as BaseController;
+
 abstract class Controller
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
